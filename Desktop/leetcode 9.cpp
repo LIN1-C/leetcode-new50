4fa1cb17205d95d3481cb104d9f7104869918643
fyl <1834934425@qq.com>
2026-03-21 11:12:00 +0800

    leetcode 9 answer

---
+++ Desktop/leetcode 9.cpp
@@ -0,0 +1,31 @@
+//模拟
+class Solution {
+private:
+    unordered_map<char, int> symbolValues = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000},
+    };
+
+public:
+    int romanToInt(string s) {
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            int value = symbolValues[s[i]];
+            if (i < n - 1 && value < symbolValues[s[i + 1]]) {
+                ans -= value;
+            } else {
+                ans += value;
+            }
+        }
+        return ans;
+    }
+};
+//unordered_map 的核心特点
+存储结构：基于哈希表实现，键（key）是唯一的，不能重复；值（value）可以重复
+使用前提：需要包含头文件 <unordered_map>
